9c4b7d472c0f2218a9566a2bff87f83ce2734076
tyna1202 <piosik.martyna@gmail.com>
2023-07-20 11:45:09 +0200

    Added App for Zadanie_domowe_2

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args){
+
+        Bug report = new Bug("Znaleziono blad nr 505. Brak dodatkowych danych.", "martyna@gmail.com", 3);
+
+        report.getAllInfo();
+        report.getEmail();
+        report.showStatus();
+        int reportPoint = report.getPoint();
+        System.out.println(reportPoint);
+
+    }
+}
